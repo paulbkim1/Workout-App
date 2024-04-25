2bc9fd3df3f7b2c55267eca9e3af303ded10fe32
paulbkim1 <paulbkim1@gmail.com>
2024-04-25 16:39:27 -0700

    update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ require("./config/mongoose.config");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
