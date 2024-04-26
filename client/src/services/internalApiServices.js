a0bffc5ce8c9a12c77414c088db1ac3942e1333e
paulbkim1 <paulbkim1@gmail.com>
2024-04-25 17:03:03 -0700

    update

--- client/src/services/internalApiServices.js
+++ client/src/services/internalApiServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthToken } from "../util/auth";
 
 const http = axios.create({
-  baseURL: "https://workout-app-server-lovat.vercel.app",
+  baseURL: "https://workout-app-server-lovat.vercel.app/api",
 });
 
 export const getAllExercises = async () => {
