14204d89f2a28e3c51a0e4a436d2e8c8a1229243
paulbkim1 <paulbkim1@gmail.com>
2024-04-26 19:14:24 -0700

    update

--- client/src/pages/MuscleGroups/MuscleGroups.jsx
+++ client/src/pages/MuscleGroups/MuscleGroups.jsx
@@ -6,30 +6,55 @@ import legs from "../../assets/legs.jpg";
 import shoulders from "../../assets/shoulders.jpg";
 import arms from "../../assets/arms.jpeg";
 import classes from "./MuscleGroups.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+const muscleGroups = [
+  { name: "Abs", image: abs, path: "abs" },
+  { name: "Arms", image: arms, path: "arms" },
+  { name: "Back", image: back, path: "back" },
+  { name: "Chest", image: chest, path: "chest" },
+  { name: "Legs", image: legs, path: "legs" },
+  { name: "Shoulders", image: shoulders, path: "shoulders" },
+];
 
 const MuscleGroups = () => {
-  const muscleGroups = [
-    { name: "Abs", image: abs, path: "abs" },
-    { name: "Arms", image: arms, path: "arms" },
-    { name: "Back", image: back, path: "back" },
-    { name: "Chest", image: chest, path: "chest" },
-    { name: "Legs", image: legs, path: "legs" },
-    { name: "Shoulders", image: shoulders, path: "shoulders" },
-  ];
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [loadedCount, setLoadedCount] = useState(0);
+
+  const handleImageLoad = () => {
+    setLoadedCount((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedCount === muscleGroups.length) {
+      setImagesLoaded(true);
+    }
+  }, [loadedCount]);
+
   return (
     <div className={classes.container}>
-      <h2>Targeted Exercises</h2>
-      <div className={classes.muscleGroupContainer}>
-        {muscleGroups.map(({ name, image, path }) => (
-          <div key={name} className={classes.muscle}>
-            <img src={image} alt={`Picture of ${name}`} />
-            <h3>{name}</h3>
-            <Link to={path} className={classes.exploreLink}>
-              <span>EXPLORE</span> <span>&rarr;</span>
-            </Link>
-          </div>
-        ))}
-      </div>
+      {!imagesLoaded ? (
+        <div className={classes.loadingSpinner}>
+          <FontAwesomeIcon icon={faSpinner} spin size="3x" />
+        </div>
+      ) : (
+        <div className={classes.muscleGroupContainer}>
+          {muscleGroups.map(({ name, image, path }) => (
+            <div key={name} className={classes.muscle}>
+              <img
+                src={image}
+                alt={`Picture of ${name}`}
+                onLoad={handleImageLoad}
+              />
+              <h3>{name}</h3>
+              <Link to={path} className={classes.exploreLink}>
+                <span>EXPLORE</span> <span>&rarr;</span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
