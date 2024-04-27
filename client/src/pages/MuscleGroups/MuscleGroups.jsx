ed3ae7b8c7a7cbdd698c5f4beb643880affb2d94
paulbkim1 <paulbkim1@gmail.com>
2024-04-26 19:09:46 -0700

    update

--- client/src/pages/MuscleGroups/MuscleGroups.jsx
+++ client/src/pages/MuscleGroups/MuscleGroups.jsx
@@ -7,16 +7,15 @@ import shoulders from "../../assets/shoulders.jpg";
 import arms from "../../assets/arms.jpeg";
 import classes from "./MuscleGroups.module.css";
 
-const muscleGroups = [
-  { name: "Abs", image: abs, path: "abs" },
-  { name: "Arms", image: arms, path: "arms" },
-  { name: "Back", image: back, path: "back" },
-  { name: "Chest", image: chest, path: "chest" },
-  { name: "Legs", image: legs, path: "legs" },
-  { name: "Shoulders", image: shoulders, path: "shoulders" },
-];
-
 const MuscleGroups = () => {
+  const muscleGroups = [
+    { name: "Abs", image: abs, path: "abs" },
+    { name: "Arms", image: arms, path: "arms" },
+    { name: "Back", image: back, path: "back" },
+    { name: "Chest", image: chest, path: "chest" },
+    { name: "Legs", image: legs, path: "legs" },
+    { name: "Shoulders", image: shoulders, path: "shoulders" },
+  ];
   return (
     <div className={classes.container}>
       <h2>Targeted Exercises</h2>
